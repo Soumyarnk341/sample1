5f67eb578436c4534b265a5ec33bedfed51b4358
Soumyarnk341 <khatuasoumyaranjan110@gmail.com>
2025-01-06 15:01:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 5;
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+if (sum > 10) {
+  console.log("Sum is greater than 10");
+} else {
+  console.log("Sum is less than or equal to 10");
+}
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
